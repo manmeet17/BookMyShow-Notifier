c063a5144469e79781b7b328d0a569911f564e77
Manmeet Tarun <meetutarun16@gmail.com>
2018-05-02 00:33:06 +0530

    Mailer added

--- book.py
+++ book.py
@@ -2,14 +2,29 @@ import requests
 from bs4 import BeautifulSoup
 import subprocess
 import time
+import smtplib
+import  os
 
+
+def send_mail(msg):
+    email=os.environ.get("email")
+    password=os.environ.get("password")
+    s=smtplib.SMTP('smtp.gmail.com',587)
+    s.starttls()
+    s.login(email,password)
+    s.sendmail(email,"meetutarun16@gmail.com",msg)
+    s.quit()
+    print (msg)
+    print("Sent")
 
 def send(msg):
     subprocess.Popen(['notify-send',msg])
     return
 
+it=0
 
 while(True):
+    it+=1
     url="https://in.bookmyshow.com/buytickets/avengers-infinity-war-hyderabad/movie-hyd-ET00053419-MT/"
     r=requests.get(url)
     soup=BeautifulSoup(r.text,'lxml')
@@ -26,8 +41,10 @@ while(True):
             t=str(i.text)
             # print (t.split())
             for j in t.split():
-                if j.strip()=="SUN" or j.strip()=="03":
+                if j.strip()=="SUN" or j.strip()=="06":
                     print (i)
+                    send_mail(msg=str(j)+" April tickets are available")
                     send(str(j)+" tickets are available")
-
-    time.sleep(5)
+    print ("Iteration :",it)
+    print ("Time:",str(it*5)+"m in")
+    time.sleep(300)
